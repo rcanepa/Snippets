e38e5aa1c79545017a2e15953d8489c448bd12e7
Renzo Canepa <rcanepag@gmail.com>
2014-11-07 18:07:06 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,50 @@
-// Sleep function for javascript
 
+// Sleep function for javascript
 function sleep(miliseconds){
   var startTime = new Date().getTime();
   while (new Date().getTime() < startTime + miliseconds);
 }
 
-// sleep(1000);
+sleep(1000);
+
+// ------------------------------------------------------------------- //
+
+// Prototype silly example
+var Vehicle = function(){};
+
+Vehicle.prototype.drive = function(){
+  console.log('vroooom');
+}
+
+var Car = function(){};
+
+// Car uses the Vehicle prototype
+Car.prototype = new Vehicle();
+
+// Car unique method
+Car.prototype.honk = function(){
+  console.log('honk hoooonk');
+}
+
+var Bike = function(){};
+
+// Bike also uses the Vehicle prototype
+Bike.prototype = new Vehicle();
+
+// Bike unique method
+Bike.prototype.ring = function(){
+  console.log('ring riiiing');
+}
+
+var myCar = new Car();
+var myBike = new Bike();
+
+myCar.drive(); // vroooom
+myCar.honk(); // honk hoooonk
+myCar.ring(); // TypeError: undefined is not a function
+
+myBike.drive(); // vroooom
+myBike.ring(); // ring riiiing
+myBike.honk(); // TypeError: undefined is not a function
+// ------------------------------------------------------------------- //
+
